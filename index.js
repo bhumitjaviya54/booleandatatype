85ec810a8767bb0649f862b2e1663d4ca081c980
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-28 12:53:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Boolean data type
+
+let learning = true;
+let completed = false;
+let x = 20 > 10;
+let y = 20 < 10;
+
+console.log(typeof learning);
+console.log(completed);
+console.log(x);
+console.log(typeof y);
